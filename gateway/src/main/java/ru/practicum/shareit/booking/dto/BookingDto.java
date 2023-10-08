e544d862ade5a347b1c0301e3a9aa2b2e9f27ed1
MariyaKud <m.kudryashova@tealfox.com>
2023-10-08 21:03:44 +0300

    #Sprint 16 microservice + add docker + check review 1

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.booking.dto;
 
 import java.time.LocalDateTime;
 
-import javax.validation.constraints.Future;
 import javax.validation.constraints.FutureOrPresent;
 
 import lombok.AllArgsConstructor;
